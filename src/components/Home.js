a4c1e6b2b6dac254f20e15065a6e3d45854073ed
Assumptaginika <ozoudeassumpta@gmail.com>
2022-10-26 10:10:17 +0100

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,30 @@
 import Hero from "./Hero";
+import Img from "../images/karl.jpg";
 
 const SearchView = () => {
   return (
     <>
       <Hero text="Welcome To Chimek Movie APP" />
+      <img
+        src={Img}
+        alt=""
+        className="homeimg"
+        style={{ width: "100%", height: "100vh", position: "relative" }}
+      />
+
+      <p
+        className="lead my-5"
+        style={{
+          position: "absolute",
+          fontSize: "30px",
+          bottom: "40px",
+          color: "white",
+          fontWeight: "bold",
+          padding: "10px",
+        }}
+      >
+        Your Home for everything movies
+      </p>
     </>
   );
 };
